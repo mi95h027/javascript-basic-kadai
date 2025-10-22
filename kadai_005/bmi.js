b7eaaec065bc68f49c26c508e964e042f2989053
mi95h027 <mymi95h027@gmail.com>
2025-10-22 19:58:47 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,7 @@
+let userWeight = 68;
+let userHeight = 1.7;
+console.log('体重' + userWeight + 'kg')
+console.log('身長' + userHeight + 'm' )
+
+let bmi = userWeight / (userHeight * userHeight);
+console.log("あなたのBMIは" + bmi + " です。");
